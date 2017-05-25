0cdc619b59ad104321f986988cf1e195418b2838
Neo <neojdpm@gmail.com>
2017-05-25 10:54:44 -0430

    Urls in /

--- nago/urls.py
+++ nago/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^', include('server.urls')),
     ## Backoffice module's URLs
-    url(r'^backoffice/', include('backoffice.urls'), name='BackofficeApp'),
+    url(r'^', include('backoffice.urls'), name='BackofficeApp'),
     ## Login/Loguot
     url(r'^accounts/login/$', auth_views.login, {'template_name': 'backoffice/login.html'}),
     url(r'^accounts/logout/$', auth_views.logout, {'next_page': 'login'})
